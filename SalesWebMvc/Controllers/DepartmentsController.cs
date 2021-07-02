13ae05863e11b13dfc5259877d66d48e9d98a39f
Ricardo <ricardogoncalvesrsg90@gmail.com>
2021-07-02 15:51:51 -0300

    Add alterações

--- SalesWebMvc/Controllers/DepartmentsController.cs
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -25,6 +25,7 @@ namespace SalesWebMvc.Controllers
             return View(await _context.Department.ToListAsync());
         }
 
+
         // GET: Departments/Details/5
         public async Task<IActionResult> Details(int? id)
         {
